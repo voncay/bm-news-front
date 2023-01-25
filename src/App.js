d8683d17779ad41c0fda5cdf88ce1b4368b0bdd6
Barbara <barbara@git>
2023-01-25 12:09:17 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function App() {
+
+  useEffect( () => {
+    axios.get('http://localhost:8000/api/articles')
+    .then(res => {
+        console.log(res, "response from App")
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BM NEWS</h1>
     </div>
   );
 }

56d92abfad1c6422307b7eb923cb8af42e985284
Barbara <barbara@git>
2023-01-26 15:58:07 +0100

    dirty fix for mapping comments

--- src/components/News.js
+++ src/components/News.js
@@ -1,30 +1,46 @@
 import "../App.css"
 import AddComment from './AddComment'
-// import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const News = ({ newsData}) =>{
 
   // console.log(newsData, "newsData from News")
 
+  const [commentsData, setCommentsData] = useState([]);
+
+  const fetchCommentsData = async () => {
+    await axios.get('http://localhost:8000/api/comments')
+    .then(res => {
+        setCommentsData(res.data)
+        console.log(res.data, "commentsData from fetch in App")
+    })
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchCommentsData();
+  }, [])
+
   return (
     <div className="news">
       <ul style={{listStyleType: 'none'}}>
-        { newsData.map((e, index) => {
+        { newsData.map((article, index) => {
             return (
-              <li key={e._id}>
-                <h3>{e.title}</h3>
-                <p>{e.text}</p>
+              <li key={article._id}>
+                <h3>{article.title}</h3>
+                <p>{article.text}</p>
                 <div>
                   <h4>Comments</h4>
-                  { e.comments.map((el, i) => {
+                  { commentsData.filter(comment =>  comment.articleId && comment.articleId._id === article._id).map((el, i) => {
                     return(
                       <div key={el._id}>
-                        <p>{el.author_name}</p>
-                        <p>{el.text}</p>
+                        <p><b>{el.author_name}</b></p>
+                        <p><em>{el.text}</em></p>
                       </div>
                     )
                   })}
-                < AddComment articleId={e._id}/>
+                < AddComment articleId={article._id}/>
                 </div>
               </li>
             )
